34c7dbdde035d93e1b389d8e6b5924c0f3a16af8
vung-epam <vu_nguyen@epam.com>
2026-05-25 14:42:35 +0700

    fix(app): update defineConfig to dynamic load base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,32 @@
-// ### app/vite.config.ts ###
+/// <reference types="vite/client" />
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import { defineConfig, loadEnv } from 'vite';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import vitePluginSingleSpa from 'vite-plugin-single-spa';
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    vitePluginSingleSpa({
-      serverPort: 4101,
-      spaEntryPoints: 'src/main.ts',
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [
+      vue(),
+      vitePluginSingleSpa({
+        serverPort: 4101,
+        spaEntryPoints: 'src/main.ts',
+      }),
+      cssInjectedByJsPlugin(),
+    ],
+    base: env.VITE_BASE_URL,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
 
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    build: {
+      rollupOptions: {
+        external: ['single-spa'],
+      },
     },
-  },
-  build: {
-    rollupOptions: {
-      external: ['single-spa'],
-    },
-  },
+  };
 });
