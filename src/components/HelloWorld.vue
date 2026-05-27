18b3fee75a6ce07d06e7d913e2aa6f485e0ab234
vung-epam <vu_nguyen@epam.com>
2026-05-27 16:30:46 +0700

    fix(app): auth

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -22,98 +22,95 @@ const count = ref(0);
 
 <template>
   <div v-if="isLoading">Loading authentication...</div>
-  <div v-else-if="isAuthenticated">
+  <template v-else-if="isAuthenticated">
     <p>Welcome, {{ user?.name }}!</p>
-  </div>
-  <div v-else>
-    <p>You are not authenticated. Please log in in /shared page.</p>
-  </div>
-  <div class="icon-sprite" v-html="iconsSprite" aria-hidden="true"></div>
-  <section id="center">
-    <div class="hero">
-      <img :src="heroImg" class="base" width="170" height="179" alt="" />
-      <img :src="vueLogo" class="framework" alt="Vue logo" />
-      <img :src="viteLogo" class="vite" alt="Vite logo" />
-    </div>
-    <div>
-      <h1>Get started</h1>
-      <p>Edit <code>src/App.vue</code> and save to test <code>HMR</code></p>
-      <p v-if="themeLabel">Theme: {{ themeLabel }}</p>
-    </div>
-    <button type="button" class="counter" @click="count++">
-      Count is {{ count }}
-    </button>
-  </section>
+    <div class="icon-sprite" v-html="iconsSprite" aria-hidden="true"></div>
+    <section id="center">
+      <div class="hero">
+        <img :src="heroImg" class="base" width="170" height="179" alt="" />
+        <img :src="vueLogo" class="framework" alt="Vue logo" />
+        <img :src="viteLogo" class="vite" alt="Vite logo" />
+      </div>
+      <div>
+        <h1>Get started</h1>
+        <p>Edit <code>src/App.vue</code> and save to test <code>HMR</code></p>
+        <p v-if="themeLabel">Theme: {{ themeLabel }}</p>
+      </div>
+      <button type="button" class="counter" @click="count++">
+        Count is {{ count }}
+      </button>
+    </section>
 
-  <div class="ticks"></div>
+    <div class="ticks"></div>
 
-  <section id="next-steps">
-    <div id="docs">
-      <svg class="icon" role="presentation" aria-hidden="true">
-        <use href="#documentation-icon"></use>
-      </svg>
-      <h2>Documentation</h2>
-      <p>Your questions, answered</p>
-      <ul>
-        <li>
-          <a href="https://vite.dev/" target="_blank">
-            <img class="logo" :src="viteLogo" alt="" />
-            Explore Vite
-          </a>
-        </li>
-        <li>
-          <a href="https://vuejs.org/" target="_blank">
-            <img class="button-icon" :src="vueLogo" alt="" />
-            Learn more
-          </a>
-        </li>
-      </ul>
-    </div>
-    <div id="social">
-      <svg class="icon" role="presentation" aria-hidden="true">
-        <use href="#social-icon"></use>
-      </svg>
-      <h2>Connect with us</h2>
-      <p>Join the Vite community</p>
-      <ul>
-        <li>
-          <a href="https://github.com/vitejs/vite" target="_blank">
-            <svg class="button-icon" role="presentation" aria-hidden="true">
-              <use href="#github-icon"></use>
-            </svg>
-            GitHub
-          </a>
-        </li>
-        <li>
-          <a href="https://chat.vite.dev/" target="_blank">
-            <svg class="button-icon" role="presentation" aria-hidden="true">
-              <use href="#discord-icon"></use>
-            </svg>
-            Discord
-          </a>
-        </li>
-        <li>
-          <a href="https://x.com/vite_js" target="_blank">
-            <svg class="button-icon" role="presentation" aria-hidden="true">
-              <use href="#x-icon"></use>
-            </svg>
-            X.com
-          </a>
-        </li>
-        <li>
-          <a href="https://bsky.app/profile/vite.dev" target="_blank">
-            <svg class="button-icon" role="presentation" aria-hidden="true">
-              <use href="#bluesky-icon"></use>
-            </svg>
-            Bluesky
-          </a>
-        </li>
-      </ul>
-    </div>
-  </section>
+    <section id="next-steps">
+      <div id="docs">
+        <svg class="icon" role="presentation" aria-hidden="true">
+          <use href="#documentation-icon"></use>
+        </svg>
+        <h2>Documentation</h2>
+        <p>Your questions, answered</p>
+        <ul>
+          <li>
+            <a href="https://vite.dev/" target="_blank">
+              <img class="logo" :src="viteLogo" alt="" />
+              Explore Vite
+            </a>
+          </li>
+          <li>
+            <a href="https://vuejs.org/" target="_blank">
+              <img class="button-icon" :src="vueLogo" alt="" />
+              Learn more
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div id="social">
+        <svg class="icon" role="presentation" aria-hidden="true">
+          <use href="#social-icon"></use>
+        </svg>
+        <h2>Connect with us</h2>
+        <p>Join the Vite community</p>
+        <ul>
+          <li>
+            <a href="https://github.com/vitejs/vite" target="_blank">
+              <svg class="button-icon" role="presentation" aria-hidden="true">
+                <use href="#github-icon"></use>
+              </svg>
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a href="https://chat.vite.dev/" target="_blank">
+              <svg class="button-icon" role="presentation" aria-hidden="true">
+                <use href="#discord-icon"></use>
+              </svg>
+              Discord
+            </a>
+          </li>
+          <li>
+            <a href="https://x.com/vite_js" target="_blank">
+              <svg class="button-icon" role="presentation" aria-hidden="true">
+                <use href="#x-icon"></use>
+              </svg>
+              X.com
+            </a>
+          </li>
+          <li>
+            <a href="https://bsky.app/profile/vite.dev" target="_blank">
+              <svg class="button-icon" role="presentation" aria-hidden="true">
+                <use href="#bluesky-icon"></use>
+              </svg>
+              Bluesky
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
 
-  <div class="ticks"></div>
-  <section id="spacer"></section>
+    <div class="ticks"></div>
+    <section id="spacer"></section>
+  </template>
 </template>
 
 <style scoped>
